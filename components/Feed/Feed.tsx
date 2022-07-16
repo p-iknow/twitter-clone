61f80ee2bbcb2390d9c26084268d09471060560f
Youngchang Lee <apricotsoul@gmail.com>
2022-07-16 18:41:19 +0900

    feat: feed (#6)
    * feat: feed-template
    
    * chore: change image src with pulbic folder
    
    * chore: add avatar-man image
    
    * chore: image extension jpeg to png
    
    * feat: feed template
    
    * feat: TweetBox
    
    resolve #5 
    
    Co-authored-by: p-iknow <apricotsoul@gmail.comm>

---
+++ components/Feed/Feed.tsx
@@ -0,0 +1,16 @@
+import { RefreshIcon } from '@heroicons/react/outline';
+import { TweetBox } from './TweetBox';
+
+export const Feed = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-between ">
+        <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
+        <RefreshIcon className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+      </div>
+      <div>
+        <TweetBox />
+      </div>
+    </div>
+  );
+};
